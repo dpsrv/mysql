ff22b00ca6da7094176affa1b10004fdf81ea0bb
Maxim Fortun <max@maxf.net>
2024-05-20 16:56:10 +0000

    updated

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@ FROM mysql:latest
 RUN microdnf install -y bind-utils
 
 COPY replication /opt/replication
+
+CMD [ "mysqld", "--mysql-native-password=ON" ]
--- mysql.sh
+++ mysql.sh
@@ -2,4 +2,5 @@
 
 . ../rc/secrets/mysql/mysql.env
 
-docker exec -it mysql mysql -h localhost -u root -p$MYSQL_ROOT_PASSWORD mysql "$@"
+CONTAINER_NAME=dpsrv-mysql-latest
+docker exec -it $CONTAINER_NAME mysql -h localhost -u root -p$MYSQL_ROOT_PASSWORD mysql "$@"
